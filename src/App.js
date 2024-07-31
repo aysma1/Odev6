0702961bb3c8b70a941717fe684da91b781ce6f2
aysma1 <168637232+aysma1@users.noreply.github.com>
2024-07-31 13:03:37 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
 const App = () => {
-  const [initialNumber, setInitialNumber] = useState(1);
-
-  const handleInputChange = (e) => {
-    setInitialNumber(Number(e.target.value));
-  };
-
   return (
-    
     <div className='App'>
-      <Counter initialNumber={initialNumber} />
-      <input
-        type='number'
-        value={initialNumber}
-        onChange={handleInputChange}
-        className='buttons'
-      />
-      
+      <Counter />
     </div>
   );
 };
