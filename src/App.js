e45e6bd64db5013283aa4685f807fcaa8f1941ab
aysma1 <168637232+aysma1@users.noreply.github.com>
2024-07-31 13:37:30 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
 const App = () => {
+  const [number, setNumber] = useState(1);
+
+  const handleInputChange = (value) => {
+    setNumber(value);
+  };
+
   return (
     <div className='App'>
-      <Counter />
+      <Counter 
+        number={number}
+        onChangeNumber={handleInputChange}
+      />
     </div>
   );
 };
